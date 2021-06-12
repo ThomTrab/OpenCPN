1b535763247e7081a08b817bb8ccda4b4c2d5b5a
ThomTrab <kheops46230@gmail.com>
2021-06-12 13:05:04 +0200

    Change Header
    Change Header of files GribUIDialogBase as these file are no longer automaticaly generated

--- plugins/grib_pi/src/GribUIDialogBase.cpp
+++ plugins/grib_pi/src/GribUIDialogBase.cpp
@@ -1,9 +1,29 @@
-///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Jun  5 2014)
-// http://www.wxformbuilder.org/
-//
-// PLEASE DO "NOT" EDIT THIS FILE!
-///////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ *
+ * Project:  OpenCPN
+ * Purpose:  GRIB GUI Definition
+ * Author:   Thomas Trabattoni
+ *
+ ***************************************************************************
+ *   Copyright (C) 2021 by Thomas Trabattoni   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ ***************************************************************************
+ *
+ */
 
 #include "GribUIDialogBase.h"
 
