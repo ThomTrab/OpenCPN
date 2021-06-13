f15758ece7677bdba37ec1b9ce42953a0a8cbd70
ThomTrab <kheops46230@gmail.com>
2021-06-13 10:17:22 +0200

    Change Header

--- plugins/grib_pi/src/GribUIDialogBase.cpp
+++ plugins/grib_pi/src/GribUIDialogBase.cpp
@@ -2,10 +2,10 @@
  *
  * Project:  OpenCPN
  * Purpose:  GRIB GUI Definition
- * Author:   Thomas Trabattoni
+ * Author:
  *
  ***************************************************************************
- *   Copyright (C) 2021 by Thomas Trabattoni   *
+ *   Copyright (C)                                                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
